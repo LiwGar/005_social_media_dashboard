67e21facd2c1903d44346a49ef4fe33a21c4a6a9
liwgar <wilenri341@hotmail.com>
2024-04-28 13:38:38 -0500

    Creacion componente header, con estilos

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -2,11 +2,23 @@ const Header = () => {
 
   return (
   
-    <header className="">
-      
+    <header className="wrapper pt-10 px-6 h-[15rem] bg-neutral-lightTheme-veryPaleBlueTopBg font-bold rounded-b-[1.25rem]">
+      <h1 className="text-[1.7rem] leading-[1.5rem] text-neutral-lightTheme-veryDarkBlueText">Social Media Dashboard</h1>
+      <p className="mt-2 mb-6 text-neutral-lightTheme-darkGrayishBlueText">Total Followers: 23,004</p>
+      <hr className="mb-[1.2rem] bg-neutral-lightTheme-veryDarkBlueText"/>
+      <div className="flex justify-between">
+        <p className="text-neutral-lightTheme-darkGrayishBlueText">Dark Mode</p>
+        <label htmlFor="darkMode" className="relative p-[0.2rem] w-[3.2rem] h-[1.6rem] bg-lightTheme-toggle rounded-full cursor-pointer peer-checked:bg-darkTheme-toggle">
+          <input id="darkMode" type="checkbox" className="sr-only peer"/>
+          <div className="absolute top-0 left-0 w-full h-full rounded-full peer-checked:bg-toggleGradient"></div>
+          <div className="w-[1.15rem] h-[1.15rem] bg-primary-white rounded-full peer-checked:bg-neutral-darkTheme-darkDesaturatedBlueCardBg peer-checked:translate-x-[1.66rem] transition-all duration-300"></div>
+        </label>
+      </div>
     </header>
     
   );
 };
 
 export default Header;
+
+/**4:11:11 */
--- tailwind.config.js
+++ tailwind.config.js
@@ -10,23 +10,21 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        inter: ["Inter", 'sans-serif']
+        Inter: ["Inter"]
       },
       colors: {
         primary: {
           limeGreen: "hsl(163, 72%, 41%)",
           brightRed: "hsl(356, 69%, 56%)",
+          white: "hsl(0, 0%, 100%)",
         
           Facebook: "hsl(208, 92%, 53%)",
           Twitter: "hsl(203, 89%, 53%)",
           Instagram: "linear gradient hsl(37, 97%, 70%) to hsl(329, 70%, 58%)",
           YouTube: "hsl(348, 97%, 39%)",
         },
-        darkTheme: {
-          Toggle: "linear gradient hsl(210, 78%, 56%) to hsl(146, 68%, 55%)",
-        },
         lightTheme: {
-          Toggle: "hsl(230, 22%, 74%)",
+          toggle: "hsl(230, 22%, 74%)",
         },
         neutral: {
           darkTheme: {
@@ -34,17 +32,19 @@ export default {
             veryDarkBlueTopBg: "hsl(232, 19%, 15%)",
             darkDesaturatedBlueCardBg: "hsl(228, 28%, 20%)",
             desaturatedBlue: "hsl(228, 34%, 66%)",
-            White: "hsl(0, 0%, 100%)",
           },
           lightTheme: {
-            WhiteBg: "hsl(0, 0%, 100%)",
             veryPaleBlueTopBg: "hsl(225, 100%, 98%)",
             lightGrayishBlueCardBg: "hsl(227, 47%, 96%)",
-            darkGrayishBlueText: "hsl(228, 12%, 44%",
+            darkGrayishBlueText: "hsl(228, 12%, 44%)",
             veryDarkBlueText: "hsl(230, 17%, 14%)",
           },
         }
-      }
+      },
+      backgroundImage: {
+        toggleGradient: "linear-gradient(to right, hsl(210, 78%, 56%), hsl(146, 68%, 55%))",
+        instagram: "linear gradient(to right, hsl(37, 97%, 70%), hsl(329, 70%, 58%))",
+      },
     },
   },
   plugins: [],
