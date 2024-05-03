aa01dbdff1a3178ccaa3d47519194f69829c1767
liwgar <wilenri341@hotmail.com>
2024-05-03 12:17:33 -0500

    Config deploy

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "./",
+  base: "https://liwgar.github.io/005_social_media_dashboard/",
 })
